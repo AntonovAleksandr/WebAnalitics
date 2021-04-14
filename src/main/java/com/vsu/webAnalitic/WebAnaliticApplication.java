530dab2c7fb1adfd4b9974926535e78931394ba0
AntonovAleksandr <zzzz1917@bk.ru>
2021-04-14 14:15:52 +0300

    first commit

---
+++ src/main/java/com/vsu/webAnalitic/WebAnaliticApplication.java
@@ -0,0 +1,13 @@
+package com.vsu.webAnalitic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAnaliticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAnaliticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsu/webAnalitic/WebAnaliticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsu.webAnalitic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAnaliticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
