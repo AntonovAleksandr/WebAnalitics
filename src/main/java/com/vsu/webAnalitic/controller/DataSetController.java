c77edf8d8d99e396252f624c883a3a1283acae10
AntonovAleksandr <zzzz1917@bk.ru>
2021-05-08 01:07:04 +0300

    controller update

--- src/main/java/com/vsu/webAnalitic/controller/DataSetController.java
+++ src/main/java/com/vsu/webAnalitic/controller/DataSetController.java
@@ -3,7 +3,7 @@ package com.vsu.webAnalitic.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/app")
-//public class DataSetController {
-//}
+@RestController
+@RequestMapping("/app")
+public class DataSetController {
+}
