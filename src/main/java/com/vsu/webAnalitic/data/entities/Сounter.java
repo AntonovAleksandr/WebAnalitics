681d4b2bc714f6b0f73efa9f8c9094a8498c6163
AntonovAleksandr <zzzz1917@bk.ru>
2021-05-08 01:06:49 +0300

    entities add

---
+++ src/main/java/com/vsu/webAnalitic/data/entities/Сounter.java
@@ -0,0 +1,22 @@
+package com.vsu.webAnalitic.data.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "counters", schema = "public")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Сounter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "value")
+    private Long value;
+}
